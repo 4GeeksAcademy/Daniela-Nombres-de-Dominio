8a366f30c1f93998d88eaf9377e3ff3033cb4041
danipadi13 <159074064+danipadi13@users.noreply.github.com>
2025-06-05 17:39:26 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+for (let index = 0; index <pronoun.length; index++) { 
+  for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+     const element = pronoun[index] + adj[j] + noun[k] + ".com";
+    console.log(element)
+      
+    }
+
+      
+
+      
+    
+      
+
+  }
+}
 };
